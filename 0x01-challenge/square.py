40c42ea2cd9ab4b33eea20ea32a47c15ef033ba4
mondel123 <adamsmonday1@gmail.com>
2023-04-16 15:46:12 -0700

    square.py

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,3 @@
+A square is a type of quadrilateral with four equal sides and four right angles. To make sure your square is perfect, you'll want to make sure that all four sides are equal in length and that all four angles measure 90 degrees. You can use a ruler or measuring tape to measure each side and a protractor to measure each angle to ensure that they are all the same.
+
+If you find that any of the sides or angles are not measuring up, you can make adjustments until they do. Once you have confirmed that all four sides and angles are equal, you can be confident that you have a perfect square!
